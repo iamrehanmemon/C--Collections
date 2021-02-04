782904903b5703068a3b4b513736c3e3033f44b4
Rehan Memon <rehan4747@gmail.com>
2021-02-04 12:29:08 +0530

    Completed intro to collection and array module

--- DaysOfWeek/Program.cs
+++ DaysOfWeek/Program.cs
@@ -19,16 +19,25 @@ namespace Pluralsight.BegCShCollections.DaysOfWeek
 				"Saturday",
 				"Sunday"
 			};
-
+			
 			Console.WriteLine("Before:");
 			foreach (string day in daysOfWeek)
 				Console.WriteLine(day);
 
 			daysOfWeek[2] = "Wednesday";
 
-			Console.WriteLine("\r\nBefore:");
+			Console.WriteLine("After:");
 			foreach (string day in daysOfWeek)
 				Console.WriteLine(day);
+
+
+			Console.WriteLine("Which Day dp you want to display ?");
+			Console.Write("(Monday = 1, etc)");
+			int iDay = int.Parse(Console.ReadLine());
+
+			string choosenDay = daysOfWeek[iDay - 1];
+			Console.WriteLine($"That day is {choosenDay}");
+			Console.ReadLine();
 		}
 	}
 }
