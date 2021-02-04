69af887a7766c396fc87075b9352babe3fc6697f
Rehan Memon <rehan4747@gmail.com>
2021-02-04 15:49:10 +0530

    Importing from a Data Source into an Array Completed

---
+++ TopTenPops/CsvReader.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Pluralsight.BegCShCollections.TopTenPops
+{
+	class CsvReader
+	{
+		private string _csvFilePath;
+
+		public CsvReader(string csvFilePath)
+		{
+			this._csvFilePath = csvFilePath;
+		}
+
+		public Country[] ReadFirstNCountries(int nCountries)
+		{
+			Country[] countries = new Country[nCountries];
+
+			//Locks the file so using statement unlocks whenever StreamReader is done
+			using (StreamReader sr = new StreamReader(_csvFilePath))
+			{
+				// read header line
+				sr.ReadLine();
+				//Looping to Readline the csv file and save it to countries array
+				for (int i = 0; i < nCountries; i++)
+				{
+					string csvLine = sr.ReadLine();
+					countries[i] = ReadCountryFromCsvLine(csvLine);
+				}
+			}
+			
+			return countries;
+		}
+
+		// Method to extract data from each Line from the CSV
+		public Country ReadCountryFromCsvLine(string csvLine)
+        {
+            string[] parts = csvLine.Split(new char[] { ',' });
+
+            string name = parts[0];
+            string code = parts[1];
+            string region = parts[2];
+            int population = int.Parse(parts[3]);
+
+            return new Country(name, code, region, population);
+        }
+
+
+    }
+}
---
+++ TopTenPops/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pluralsight.BegCShCollections.TopTenPops
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string filePath = @"E:\Capgemini\Azure Training\C# Collections\TopTenPops\Pop by Largest Final.csv";
+			CsvReader reader = new CsvReader(filePath);
+
+			Country[] countries = reader.ReadFirstNCountries(10);
+
+			foreach (Country country in countries)
+			{
+				Console.WriteLine($"{PopulationFormatter.FormatPopulation(country.Population).PadLeft(15)}: {country.Name}");
+				
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
