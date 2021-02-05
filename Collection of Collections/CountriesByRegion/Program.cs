aa7c4a097565b37a8496dc6df32c4e5af7cf3194
Rehan Memon <rehan4747@gmail.com>
2021-02-05 15:56:46 +0530

    Completed Creating Collections of Collections

---
+++ Collection of Collections/CountriesByRegion/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pluralsight.BegCShCollections.ReadAllCountries
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string filePath = @"E:\Capgemini\Azure Training\C# Collections\Pop by Largest Final.csv";
+			CsvReader reader = new CsvReader(filePath);
+
+			Dictionary<string, List<Country>> countries = reader.ReadAllCountries();
+
+			foreach (string region in countries.Keys)
+				Console.WriteLine(region);
+
+			Console.Write("Which of the above regions do you want? ");
+			string chosenRegion = Console.ReadLine();
+
+			if (countries.ContainsKey(chosenRegion))
+			{
+				// display 10 highest population countries in the selected region
+				foreach (Country country in countries[chosenRegion].Take(10))
+					Console.WriteLine($"{PopulationFormatter.FormatPopulation (country.Population).PadLeft(15)}: {country.Name}");
+			}
+			else
+				Console.WriteLine("That is not a valid region");
+
+			Console.ReadLine();
+		}
+	}
+}
---
+++ Collection of Collections/NoughtsCrosses/Square.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoughtsCrosses
+{
+	public enum Player { None = 0, Noughts, Crosses }
+
+	//Starts Empty as default
+	public struct Square
+	{
+		public Square(Player owner)
+		{
+			this.Owner = owner;
+		}
+
+		public Player Owner { get; }
+
+		public override string ToString()
+		{
+			switch (Owner)
+			{
+				case Player.None:
+					return ".";
+				case Player.Crosses:
+					return "X";
+				case Player.Noughts:
+					return "O";
+				default:
+					throw new Exception("Invalid state");
+			}
+		}
+	}
+}
+
